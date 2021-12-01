238f77db5040baea715cf006f2ee6b2f2ad76570
Andreas Johansson <andreas.johansson@cygni.se>
2021-12-01 14:21:25 +0100

    fine tune

--- day01/index.js
+++ day01/index.js
@@ -7,16 +7,12 @@ function getSolutionPart1() {
     let input = inputDataLinesIntegers();
     let comp = input.slice();
     comp.unshift(input[0]);
-
     let val = input.map((val, i) => val > comp[i] ? 1 : 0).reduce((x,y) => x+y);
-    
     return val;
 }
 
 function getSolutionPart2() {
     let input = inputDataLinesIntegers();
-    
-
     let val = input.slice(0,input.length-3).map((val, i) => input.slice(i, i + 3).reduce((x,y)=>x+y) < input.slice(i + 1, i + 4).reduce((x,y)=>x+y) ? 1 : 0).reduce((x,y) => x+y);
     console.debug(val);
     return val;
