6d171488f87a2d4c6c0b34c34b72732458318304
Andreas Johansson <andreas.johansson@cygni.se>
2021-12-01 14:12:17 +0100

    prt2

--- day01/index.js
+++ day01/index.js
@@ -17,11 +17,11 @@ function getSolutionPart1() {
 
 function getSolutionPart2() {
     let input = inputDataLinesIntegers();
-    input.push(input[input.length-1],input[input.length-1],input[input.length-1]);
+    
 
-    let val = console.debug(input.slice(0,input.length-3).map((val, i) => input.slice(i, 3).reduce((x,y)=>x+y) < input.slice(i + 1, 3).reduce((x,y)=>x+y) ? 1 : 0).reduce((x,y) => x+y));
+    let val = input.slice(0,input.length-3).map((val, i) => input.slice(i, i + 3).reduce((x,y)=>x+y) < input.slice(i + 1, i + 4).reduce((x,y)=>x+y) ? 1 : 0).reduce((x,y) => x+y);
     console.debug(val);
-    return 0;
+    return val;
 }
 const part = process.env.part || "part1"
 if (part === "part1")
