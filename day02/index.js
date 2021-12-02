175efd2ff7ac55edad7f2eddb951cdafd6ddd714
Andreas Johansson <andreas.johansson@cygni.se>
2021-12-02 23:52:47 +0100

    shorten

--- day02/index.js
+++ day02/index.js
@@ -1,58 +1,15 @@
-const { Console } = require('console');
-const fs = require('fs')
-function inputDataLinesIntegers(filename="input.txt") {
-    let input = fs.readFileSync(filename).toString().trim().split("\n");
-    
-    return input;
-}
+const fileinput = require('fs').readFileSync('input.txt').toString().trim().split("\n");
 
 function getSolutionPart1() {
-    let input = inputDataLinesIntegers();    
     var res = [0, 0];
-
-    input.forEach(x => {
-        let dir = x.split(' ')[0];
-        let val = parseInt(x.split(' ')[1]);
-        if(dir === 'forward' ) {
-            res[0] += val;
-        } else if (dir === 'down') {
-            res[1] += val;
-        } else {
-            res[1] -= val;
-        }
-    }
-        );
+    fileinput.forEach(x => { let dir = x.split(' ')[0]; let val = parseInt(x.split(' ')[1]); if(dir === 'forward' ) { res[0] += val; } else if (dir === 'down') {res[1] += val;} else {res[1] -= val;}});
     return res[0] * res[1];
 }
 
 function getSolutionPart2() {
-    let input = inputDataLinesIntegers();    
     var res = [0, 0, 0];
-
-    input.forEach(x => {
-        let dir = x.split(' ')[0];
-        let val = parseInt(x.split(' ')[1]);
-        if(dir === 'forward' ) {
-            res[0] += val;
-            res[1] += val * res[2];
-        } else if (dir === 'down') {
-            res[2] += val;
-        } else {
-            res[2] -= val;
-        }
-        
-    }
-        
-        );
+    fileinput.forEach(x => {let dir = x.split(' ')[0]; let val = parseInt(x.split(' ')[1]);if(dir === 'forward' ) {res[0] += val; res[1] += val * res[2];} else if (dir === 'down') {res[2] += val;} else {res[2] -= val;}});
     return res[0] * res[1];
 }
-const part = process.env.part || "part1"
 
-if (part === "part1")
-    console.log(getSolutionPart1())
-else
-    console.log(getSolutionPart2())
-
-module.exports = {
-    getSolutionPart1, getSolutionPart2, inputDataLinesIntegers
-}
+if (process.env.part || "part1" === "part1") {console.log(getSolutionPart1())} else {console.log(getSolutionPart2())}
