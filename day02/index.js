82d304fede127906ebef78215064e2c99e3db5da
Andreas Johansson <andreas.johansson@cygni.se>
2021-12-02 23:56:27 +0100

    shorten

--- day02/index.js
+++ day02/index.js
@@ -1,14 +1,12 @@
-const fileinput = require('fs').readFileSync('input.txt').toString().trim().split("\n");
-
 function getSolutionPart1() {
     var res = [0, 0];
-    fileinput.forEach(x => { let dir = x.split(' ')[0]; let val = parseInt(x.split(' ')[1]); if(dir === 'forward' ) { res[0] += val; } else if (dir === 'down') {res[1] += val;} else {res[1] -= val;}});
+    require('fs').readFileSync('input.txt').toString().trim().split("\n").forEach(x => { let dir = x.split(' ')[0]; let val = parseInt(x.split(' ')[1]); if(dir === 'forward' ) { res[0] += val; } else if (dir === 'down') {res[1] += val;} else {res[1] -= val;}});
     return res[0] * res[1];
 }
 
 function getSolutionPart2() {
     var res = [0, 0, 0];
-    fileinput.forEach(x => {let dir = x.split(' ')[0]; let val = parseInt(x.split(' ')[1]);if(dir === 'forward' ) {res[0] += val; res[1] += val * res[2];} else if (dir === 'down') {res[2] += val;} else {res[2] -= val;}});
+    require('fs').readFileSync('input.txt').toString().trim().split("\n").forEach(x => {let dir = x.split(' ')[0]; let val = parseInt(x.split(' ')[1]);if(dir === 'forward' ) {res[0] += val; res[1] += val * res[2];} else if (dir === 'down') {res[2] += val;} else {res[2] -= val;}});
     return res[0] * res[1];
 }
 
