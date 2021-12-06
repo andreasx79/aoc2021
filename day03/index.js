84ee31677ad50b66ee435d9fca3700840246520f
Andreas Johansson <andreas.johansson@cygni.se>
2021-12-06 10:43:25 +0100

    part2

--- day03/index.js
+++ day03/index.js
@@ -1,3 +1,4 @@
+const { connect } = require('http2');
 
 function inputDataLinesIntegers(filename="input.txt") {
     return require('fs').readFileSync(filename).toString().trim().split("\n");
@@ -8,46 +9,51 @@ const part = process.env.part || "part1"
 function getSolution() {
     let input = inputDataLinesIntegers();
 
-    let numbers = [0,0,0,0,0,0,0,0,0,0,0,0];
+    if(part === "part11") {
+        let numbers = [0,0,0,0,0,0,0,0,0,0,0,0];
     
-    input.forEach((val, i) => val.split("").forEach((x, idc) => numbers[idc] += parseInt(x)));
-    let gammastr = '';
-    let epsStr = '';
-    for(let i=0; i<12; i++) {
-        if( numbers[i] > input.length/2) {
-            gammastr += "1";
-            epsStr += "0";
-        } else {
-            gammastr += "0";
-            epsStr += "1";
-        }
-    }
-
-    if(part === "part1")
-        return parseInt(gammastr, 2) * parseInt(epsStr, 2);
-    else {
-        let gstr = estr = '';
-        input = input.sort();
-
-        for (let i = 0; i<12; i++) {
-            if(input.filter(x => x.startsWith(gstr)).length === 1) break;
-            if(input.filter(x => x.startsWith(gstr + '1')).length >= input.filter(x => x.startsWith(gstr + '0')).length) {
-                gstr += '1';
+        input.forEach((val, i) => val.split("").forEach((x, idc) => numbers[idc] += parseInt(x)));
+    
+        let gammastr = '';
+        let epsStr = '';
+        for(let i=0; i<12; i++) {
+            if( numbers[i] > input.length/2) {
+                gammastr += "1";
+                epsStr += "0";
             } else {
-                gstr += '0';
+                gammastr += "0";
+                epsStr += "1";
             }
         }
-        
+            return parseInt(gammastr, 2) * parseInt(epsStr, 2);
+
+    } else {
+        let oxygen = CO2 = '';
+        let inputdata = input;
+
         for (let i = 0; i<12; i++) {
-            if(input.filter(x => x.startsWith(estr)).length === 1) break;
-            if(input.filter(x => x.startsWith(estr + '0')).length <= input.filter(x => x.startsWith(estr + '1')).length) {
-                estr += '0';
+            
+            if(inputdata.length === 1) break;
+            if(inputdata.filter(x => x.startsWith(oxygen + '1')).length >= inputdata.filter(x => x.startsWith(oxygen + '0')).length) {
+                oxygen += '1';
             } else {
-                estr += '1';
+                oxygen += '0';
             }
+            inputdata = inputdata.filter(x => x.startsWith(oxygen)).sort();
+        }
+        inputdata = input;
+        for (let i = 0; i<12; i++) {
+            
+            if(inputdata.length === 1) break;
+            if(inputdata.filter(x => x.startsWith(CO2 + '0')).length <= inputdata.filter(x => x.startsWith(CO2 + '1')).length) {
+                CO2 += '0';
+            } else {
+                CO2 += '1';
+            }
+            inputdata = inputdata.filter(x => x.startsWith(CO2)).sort();
         }
 
-        return parseInt(gstr, 2) * parseInt(estr, 2);
+        return parseInt(input.filter(x=>x.startsWith(oxygen))[0], 2) * parseInt(input.filter(x=>x.startsWith(CO2))[0], 2);
     }
 }
 
